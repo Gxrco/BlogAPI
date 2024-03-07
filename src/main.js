efeb018e83646b8ccfd7f039f8656b95410a9547
Gerson Ramirez <ram22281@uvg.edu.gt>
2024-03-07 09:35:31 -0600

    refactor: connect Swagger documentation

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
-import express from 'express'
-import fs from 'fs'
+import express from 'express';
+import fs from 'fs';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 import {
   getAllPosts, createPost, getPostById, updatePost, deletePost,
 } from './db.js'
 
 const app = express()
 app.use(express.json())
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const swaggerDocument = YAML.load(join(__dirname, '../docs/swagger/swagger.yaml'));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Middleware para loggear las peticiones
 app.use((req, res, next) => {
