d3d6d35d67d0e843fb11d48781602644cfa462bc
Gerson Ramirez <ram22281@uvg.edu.gt>
2024-03-06 20:11:44 -0600

    style: format code following guideline

--- src/conn.js
+++ src/conn.js
@@ -1,23 +1,23 @@
-import mysql from 'mysql2/promise';
+import mysql from 'mysql2/promise'
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'gerco',
-    database: 'blog_db',
-    password: '1234',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+  host: 'localhost',
+  user: 'gerco',
+  database: 'blog_db',
+  password: '1234',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+})
 
 pool.getConnection()
-    .then(connection => {
-        console.log('Connected to MySQL database successfully');
-        connection.release();
-    })
-    .catch(error => {
-        console.error('Error connecting to MySQL database:', error);
-        process.exit(1); 
-    });
+  .then((connection) => {
+    console.log('Connected to MySQL database successfully')
+    connection.release()
+  })
+  .catch((error) => {
+    console.error('Error connecting to MySQL database:', error)
+    process.exit(1)
+  })
 
-export default pool;
+export default pool
