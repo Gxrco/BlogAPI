05a6a4eb847b75c3e719ad9f220349ccfdbd6821
Gerson Ramirez <ram22281@uvg.edu.gt>
2024-03-14 17:33:46 -0600

    chore: change credentials

--- src/conn.js
+++ src/conn.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise'
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'gerco',
-  database: 'blog_db',
+  database: 'blog_gerco',
   password: '1234',
   waitForConnections: true,
   connectionLimit: 10,
